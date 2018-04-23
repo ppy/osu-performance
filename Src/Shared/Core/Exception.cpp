ef7b93cde338f1ddcb46069ea4eee08a78fa8660
Thomas Müller <thomas94@gmx.net>
2018-04-23 06:28:48 +0200

    Fix variable format in Exception.cpp/.h

--- Src/Shared/Core/Exception.cpp
+++ Src/Shared/Core/Exception.cpp
@@ -1,22 +1,22 @@
 #include <Shared.h>
 
-CException::CException(std::string File, s32 Line, std::string Description)
-: m_File{std::move(File)}, m_Line{Line}, m_Description{std::move(Description)}
+CException::CException(std::string file, s32 line, std::string description)
+: _file{std::move(file)}, _line{line}, _description{std::move(description)}
 {
 }
 
 void CException::Print()
 {
-	std::cerr << StrFormat("Exception in: {0}:{1} - {2}\n", m_File, m_Line, m_Description);
+	std::cerr << StrFormat("Exception in: {0}:{1} - {2}\n", _file, _line, _description);
 }
 
-CLoggedException::CLoggedException(std::string File, s32 Line, std::string Description)
-: CException{std::move(File), Line, std::move(Description)}
+CLoggedException::CLoggedException(std::string file, s32 line, std::string description)
+: CException{std::move(file), line, std::move(description)}
 {
 	//Log();
 }
 
 void CLoggedException::Log()
 {
-	::Log(CLog::EType::Exception, StrFormat("{0}:{1} - {2}", m_File, m_Line, m_Description));
+	::Log(CLog::EType::Exception, StrFormat("{0}:{1} - {2}", _file, _line, _description));
 }
