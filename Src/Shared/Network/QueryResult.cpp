396098893d11386da603a4e503cb1eb044a0b1fb
Thomas Müller <thomas94@gmx.net>
2018-05-03 22:23:38 +0200

    Use std::unique_ptr with custom deleter for QueryResult
    Allows getting rid of custom move constructors

--- Src/Shared/Network/QueryResult.cpp
+++ Src/Shared/Network/QueryResult.cpp
@@ -3,47 +3,16 @@
 #include "QueryResult.h"
 
 QueryResult::QueryResult(MYSQL_RES* pRes)
-: _pRes(pRes), _Row(nullptr)
+: _pRes{pRes, &mysql_free_result}, _row{nullptr}
 {
-}
-
-QueryResult::QueryResult(QueryResult&& other)
-{
-	if (_pRes)
-		mysql_free_result(_pRes);
-
-	_pRes = other._pRes;
-	_Row = other._Row;
-
-	other._pRes = 0;
-}
-
-QueryResult& QueryResult::operator=(QueryResult&& other)
-{
-	if (_pRes)
-		mysql_free_result(_pRes);
-
-	_pRes = other._pRes;
-	_Row = other._Row;
-
-	other._pRes = 0;
-
-	return *this;
-}
-
-QueryResult::~QueryResult()
-{
-	// Clean up properly!
-	if (_pRes)
-		mysql_free_result(_pRes);
 }
 
 bool QueryResult::NextRow()
 {
 	// Don't bother executing the SQL func... we don't have a valid result anyways!
-	if (_pRes == NULL)
+	if (!_pRes)
 		return false;
 
 	// Return wether we actually HAVE any more (or any) rows
-	return ((_Row = mysql_fetch_row(_pRes)) != NULL);
+	return ((_row = mysql_fetch_row(_pRes.get())) != nullptr);
 }
--- Src/Shared/Network/QueryResult.h
+++ Src/Shared/Network/QueryResult.h
@@ -35,37 +35,29 @@ inline double xtod(const char *str)
 class QueryResult
 {
 public:
-	QueryResult(QueryResult&& other);
-	QueryResult(QueryResult& other) = delete;
-
-	QueryResult& operator=(QueryResult&& other);
-	QueryResult& operator=(QueryResult& other) = delete;
-
-	~QueryResult();
-
 	bool NextRow();
 
-	inline s32 NumRows() { return (s32)mysql_num_rows(_pRes); }
+	inline s32 NumRows() { return (s32)mysql_num_rows(_pRes.get()); }
 
 	// Entire current row - array of zero terminated strings
-	inline char** CurrentRow() { return _Row; }
+	inline char** CurrentRow() { return _row; }
 
 	// Column entries of the current row
-	inline bool IsNull(u32 i) { return _Row[i] == nullptr; }
-	inline char* String(u32 i) const { return (char*)_Row[i]; }
-	inline bool Bool(u32 i) const { return (xtoi(_Row[i]) != 0); }
-	inline s32 S32(u32 i) const { return xtoi(_Row[i]); }
-	inline u32 U32(u32 i) const { return u32(xtou(_Row[i])); }
-	inline s64 S64(u32 i) const { return xtoi64(_Row[i]); }
-	inline u64 U64(u32 i) const { return xtou64(_Row[i]); }
-	inline f32 F32(u32 i) const { return xtof(_Row[i]); }
-	inline f64 F64(u32 i) const { return xtod(_Row[i]); }
+	inline bool IsNull(u32 i) { return _row[i] == nullptr; }
+	inline char* String(u32 i) const { return (char*)_row[i]; }
+	inline bool Bool(u32 i) const { return (xtoi(_row[i]) != 0); }
+	inline s32 S32(u32 i) const { return xtoi(_row[i]); }
+	inline u32 U32(u32 i) const { return u32(xtou(_row[i])); }
+	inline s64 S64(u32 i) const { return xtoi64(_row[i]); }
+	inline u64 U64(u32 i) const { return xtou64(_row[i]); }
+	inline f32 F32(u32 i) const { return xtof(_row[i]); }
+	inline f64 F64(u32 i) const { return xtod(_row[i]); }
 
 private:
 	QueryResult(MYSQL_RES* pRes);
 
-	MYSQL_RES* _pRes;
-	MYSQL_ROW  _Row;
+	std::unique_ptr<MYSQL_RES, decltype(&mysql_free_result)> _pRes;
+	MYSQL_ROW  _row;
 
 	friend class DatabaseConnection;
 };
