700ea54b1431302c749762a17b6ca44c84597588
Thomas Müller <thomas94@gmx.net>
2018-05-06 10:41:08 +0200

    Capitalize all CLI options

--- Src/Processor/main.cpp
+++ Src/Processor/main.cpp
@@ -82,14 +82,14 @@ int main(s32 argc, char* argv[])
 		{
 			args::Flag continueFlag{
 				parser,
-				"continue",
+				"CONTINUE",
 				"Continue where a previously aborted 'all' run left off.",
 				{'c', "continue"},
 			};
 
 			args::ValueFlag<u32> threadsFlag{
 				parser,
-				"threads",
+				"THREADS",
 				"Number of threads to use. Can be useful even if the processor itself has no "
 				"parallelism due to additional connections to the database.\n"
 				"Default: 1",
