80d4bd60904f5e0a6838c681274aa1d75471f485
Thomas Müller <thomas94@gmx.net>
2018-05-17 08:13:52 +0200

    Even nicer nicer syntax

--- Src/Processor/Processor.cpp
+++ Src/Processor/Processor.cpp
@@ -81,7 +81,7 @@ void Processor::MonitorNewScores()
 	if (!res.NextRow())
 		throw ProcessorException(SRC_POS, "Couldn't find maximum approved date.");
 
-	_lastApprovedDate = res.Get<std::string>(0);
+	_lastApprovedDate = (std::string)res[0];
 
 	std::thread beatmapPollThread{[this]()
 	{
@@ -152,8 +152,8 @@ void Processor::ProcessAllUsers(bool reProcess, u32 numThreads)
 	if (!res.NextRow())
 		throw ProcessorException(SRC_POS, "Could not find user ID stats.");
 
-	const s64 maxUserId = res.IsNull(0) ? 0 : res.Get<s64>(0);
-	const s64 numUsers = res.Get<s64>(1);
+	const s64 maxUserId = res.IsNull(0) ? 0 : (s64)res[0];
+	const s64 numUsers = res[1];
 
 	s64 numUsersProcessed = 0;
 	u32 currentConnection = 0;
@@ -173,7 +173,7 @@ void Processor::ProcessAllUsers(bool reProcess, u32 numThreads)
 
 		while (res.NextRow())
 		{
-			s64 userId = res.Get<s64>(0);
+			s64 userId = res[0];
 
 			threadPool.EnqueueTask(
 				[&, userId, currentConnection]()
@@ -235,7 +235,7 @@ void Processor::ProcessUsers(const std::vector<std::string>& userNames)
 	std::vector<s64> userIds;
 	for (const auto& name : userNames)
 	{
-		s64 id = xtoi64(name.c_str());
+		s64 id = strtoll(name.c_str(), 0, 0);
 		if (id == 0)
 		{
 			// If the given string is not a number, try treating it as a username
@@ -247,7 +247,7 @@ void Processor::ProcessUsers(const std::vector<std::string>& userNames)
 			if (!res.NextRow())
 				continue;
 
-			id = res.Get<s64>(0);
+			id = res[0];
 		}
 
 		userIds.emplace_back(id);
@@ -311,7 +311,7 @@ void Processor::ProcessUsers(const std::vector<s64>& userIds)
 		));
 
 		if (res.NextRow())
-			name = res.Get<std::string>(0);
+			name = (std::string)res[0];
 
 		Log(Info, StrFormat(
 			"{0w16ar}  {1w8ar}  {2w5ar}pp  {3w6arp2} %",
@@ -415,8 +415,8 @@ void Processor::queryAllBeatmapDifficulties(u32 numThreads)
 	if (!res.NextRow())
 		throw ProcessorException(SRC_POS, "Could not find beatmap ID stats.");
 
-	const s32 maxBeatmapId = res.Get(0);
-	const s32 numBeatmaps = res.Get(1);
+	const s32 maxBeatmapId = res[0];
+	const s32 numBeatmaps = res[1];
 
 	std::vector<std::shared_ptr<DatabaseConnection>> dbSlaveConnections;
 	for (u32 i = 0; i < numThreads; ++i)
@@ -472,17 +472,17 @@ bool Processor::queryBeatmapDifficulty(DatabaseConnection& dbSlave, s32 startId,
 
 	while (res.NextRow())
 	{
-		s32 id = res.Get(0);
+		s32 id = res[0];
 
 		if (_beatmaps.count(id) == 0)
 			_beatmaps.emplace(std::make_pair(id, id));
 
 		auto& beatmap = _beatmaps.at(id);
 
-		beatmap.SetRankedStatus(res.Get(5));
-		beatmap.SetScoreVersion(res.Get(6));
-		beatmap.SetNumHitCircles(res.IsNull(1) ? 0 : res.Get<s32>(1));
-		beatmap.SetDifficultyAttribute(res.Get(2), _difficultyAttributes[res.Get<s32>(3)], res.Get(4));
+		beatmap.SetRankedStatus(res[5]);
+		beatmap.SetScoreVersion(res[6]);
+		beatmap.SetNumHitCircles(res.IsNull(1) ? 0 : (s32)res[1]);
+		beatmap.SetDifficultyAttribute(res[2], _difficultyAttributes[(size_t)res[3]], res[4]);
 	}
 
 	if (endId != 0) {
@@ -542,8 +542,8 @@ void Processor::pollAndProcessNewScores()
 		if (!res.IsNull(2))
 			continue;
 
-		s64 ScoreId = res.Get(0);
-		s64 UserId = res.Get(1);
+		s64 ScoreId = res[0];
+		s64 UserId = res[1];
 
 		_currentScoreId = std::max(_currentScoreId, ScoreId);
 
@@ -591,8 +591,8 @@ void Processor::pollAndProcessNewBeatmapSets(DatabaseConnection& dbSlave)
 
 	while (res.NextRow())
 	{
-		_lastApprovedDate = res.Get<std::string>(1);
-		queryBeatmapDifficulty(dbSlave, res.Get(0));
+		_lastApprovedDate = (std::string)res[1];
+		queryBeatmapDifficulty(dbSlave, res[0]);
 
 		_pDataDog->Increment("osu.pp.difficulty.required_retrieval", 1, { StrFormat("mode:{0}", GamemodeTag(_gamemode)) });
 	}
@@ -609,7 +609,7 @@ void Processor::queryBeatmapBlacklist()
 	));
 
 	while (res.NextRow())
-		_blacklistedBeatmapIds.insert(res.Get(0));
+		_blacklistedBeatmapIds.insert(res[0]);
 
 	Log(Success, StrFormat("Retrieved {0} blacklisted beatmaps.", _blacklistedBeatmapIds.size()));
 }
@@ -623,11 +623,11 @@ void Processor::queryBeatmapDifficultyAttributes()
 	auto res = _pDBSlave->Query("SELECT `attrib_id`,`name` FROM `osu_difficulty_attribs` WHERE 1 ORDER BY `attrib_id` DESC");
 	while (res.NextRow())
 	{
-		u32 id = res.Get(0);
+		u32 id = res[0];
 		if (_difficultyAttributes.size() < id + 1)
 			_difficultyAttributes.resize(id + 1);
 
-		_difficultyAttributes[id] = Beatmap::DifficultyAttributeFromName(res.Get(1));
+		_difficultyAttributes[id] = Beatmap::DifficultyAttributeFromName(res[1]);
 		++numEntries;
 	}
 
@@ -703,10 +703,10 @@ User Processor::processSingleUserGeneric(
 		// Process the data we got
 		while (res.NextRow())
 		{
-			s64 scoreId = res.Get(0);
-			s32 beatmapId = res.Get(2);
+			s64 scoreId = res[0];
+			s32 beatmapId = res[2];
 
-			EMods mods = res.Get(11);
+			EMods mods = res[11];
 
 			// Blacklisted maps don't count
 			if (_blacklistedBeatmapIds.count(beatmapId) > 0)
@@ -747,16 +747,16 @@ User Processor::processSingleUserGeneric(
 			TScore score = TScore{
 				scoreId,
 				_gamemode,
-				res.Get(1), // user_id
+				res[1], // user_id
 				beatmapId,
-				res.Get(3), // score
-				res.Get(4), // maxcombo
-				res.Get(5), // Num300
-				res.Get(6), // Num100
-				res.Get(7), // Num50
-				res.Get(8), // NumMiss
-				res.Get(9), // NumGeki
-				res.Get(10), // NumKatu
+				res[3], // score
+				res[4], // maxcombo
+				res[5], // Num300
+				res[6], // Num100
+				res[7], // Num50
+				res[8], // NumMiss
+				res[9], // NumGeki
+				res[10], // NumKatu
 				mods,
 				beatmap,
 			};
@@ -767,7 +767,7 @@ User Processor::processSingleUserGeneric(
 			{
 				// Column 12 is the pp value of the score from the database.
 				// Only update score if it differs a lot!
-				if (res.IsNull(12) || fabs(res.Get<f32>(12) - score.TotalValue()) > 0.001f)
+				if (res.IsNull(12) || fabs((f32)res[12] - score.TotalValue()) > 0.001f)
 					scoresThatNeedDBUpdate.emplace_back(score);
 			}
 		}
@@ -807,7 +807,7 @@ User Processor::processSingleUserGeneric(
 			if (res.IsNull(0))
 				continue;
 
-			f64 ratingChange = userPPRecord.Value - res.Get<f32>(0);
+			f64 ratingChange = userPPRecord.Value - (f64)res[0];
 
 			// We don't want to log scores, that give less than a mere 5 pp
 			if (ratingChange < s_notableEventRatingDifferenceMinimum)
@@ -866,7 +866,7 @@ s64 Processor::retrieveCount(DatabaseConnection& db, std::string key)
 
 	while (res.NextRow())
 		if (!res.IsNull(0))
-			return res.Get(0);
+			return res[0];
 
 	throw ProcessorException{SRC_POS, StrFormat("Unable to retrieve count '{0}'.", key)};
 }
--- Src/Shared/Network/QueryResult.h
+++ Src/Shared/Network/QueryResult.h
@@ -4,36 +4,6 @@
 
 DEFINE_LOGGED_EXCEPTION(QueryResultException);
 
-inline int xtoi(const char *str)
-{
-	return ::atoi(str);
-}
-
-inline u32 xtou(const char *str)
-{
-	return ::strtoul(str, 0, 0);
-}
-
-inline s64 xtoi64(const char *str)
-{
-	return ::strtoull(str, 0, 0);
-}
-
-inline u64 xtou64(const char *str)
-{
-	return ::strtoull(str, 0, 0);
-}
-
-inline float xtof(const char *str)
-{
-	return static_cast<float>(::atof(str));
-}
-
-inline double xtod(const char *str)
-{
-	return ::atof(str);
-}
-
 class QueryResult
 {
 public:
@@ -55,14 +25,21 @@ private:
 		Field(char* data) : _data{data} {}
 
 		explicit operator const char*() const { return _data; }
-		operator std::string() const { return static_cast<const char*>(*this); }
-		operator s32() const { return xtoi(_data); }
-		operator u32() const { return xtou(_data); }
-		operator s64() const { return xtoi64(_data); }
-		operator u64() const { return xtou64(_data); }
-		operator f32() const { return xtof(_data); }
-		operator f64() const { return xtod(_data); }
-		operator bool() const { return static_cast<s32>(*this) != 0; }
+		operator std::string() const { return (const char*)*this; }
+
+		operator s32() const { return atoi(_data); }
+		operator u32() const { return strtoul(_data, 0, 0); }
+
+		operator long() const { return strtol(_data, 0, 0); }
+		operator unsigned long() const { return strtoul(_data, 0, 0); }
+
+		operator s64() const { return strtoll(_data, 0, 0); }
+		operator u64() const { return strtoull(_data, 0, 0); }
+
+		operator f32() const { return (f32)(f64)*this; }
+		operator f64() const { return atof(_data); }
+
+		operator bool() const { return (s32)*this != 0; }
 
 		template<typename T, std::enable_if_t<std::is_enum<bare_type_t<T>>::value>...>
 		operator T(){
@@ -74,15 +51,12 @@ private:
 	};
 
 public:
-	Field Get(u32 i) const
+	Field operator[](size_t i) const
 	{
 		if (IsNull(i))
 			throw QueryResultException{SRC_POS, StrFormat("Attempted to interpret null result at {0}.", i)};
 		return Field{_row[i]};
 	}
-
-	template <typename T>
-	T Get(u32 i) const { return static_cast<T>(Get(i)); }
 
 private:
 	QueryResult(MYSQL_RES* pRes);
