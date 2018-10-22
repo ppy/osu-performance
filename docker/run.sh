b9638fb70f4c5ad961da77313ce768c7f417536b
smoogipoo <smoogipoo@smgi.me>
2018-10-22 19:00:23 +0900

    Add docker files

---
+++ docker/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd ..
+
+mkdir -p Build
+
+cd Build
+cmake ..
+make -j
+
+cd ../bin
+
+echo "{
+    \"mysql.master.host\" : \"${DB_HOST}\",
+    \"mysql.master.port\" : ${DB_PORT},
+    \"mysql.master.username\" : \"${DB_USER}\",
+    \"mysql.master.password\" : \"\",
+    \"mysql.master.database\" : \"osu\"
+}" > config.json
+
+./osu-performance all -t ${THREADS}
