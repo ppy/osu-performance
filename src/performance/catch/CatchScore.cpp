adec48ea912ca9dfac05cfdc48e3e1ed7003d22a
smoogipoo <smoogipoo@smgi.me>
2020-09-03 19:45:45 +0900

    Update with bastoo's latest changes

--- src/performance/catch/CatchScore.cpp
+++ src/performance/catch/CatchScore.cpp
@@ -38,7 +38,7 @@ CatchScore::CatchScore(
 	// Longer maps with more movements are worth more
 	f32 lengthBonusFactor = static_cast<f32>(numTotalHits) * 0.75f + beatmap.DifficultyAttribute(_mods, Beatmap::DirectionChangeCount) / 1.8f;
 
-	f32 lengthBonus = log10(lengthBonusFactor + 100.0f) * 0.75f - 1.3f;
+	f32 lengthBonus = log10(lengthBonusFactor + 100.0f) * 0.7f - 1.3f;
 
 	// Longer maps are worth more
 	_value *= lengthBonus;
@@ -64,7 +64,7 @@ CatchScore::CatchScore(
 
 	if ((_mods & EMods::Hidden) > 0)
 	{
-		_value *= 1.0f + 0.175f * (9.8f - std::min(9.8f, approachRate)); // 20% for each AR below 9.8
+		_value *= 1.05f + 0.17f * (10f - std::min(10f, approachRate)); // 20% for each AR below 10
 	}
 
 	if ((_mods & EMods::Flashlight) > 0)
@@ -73,9 +73,9 @@ CatchScore::CatchScore(
 		_value *= 1.35f * lengthBonus;
 
 		if (approachRate > 8.0f)
-			approachRateFactor += 0.1f * (approachRateFactor - 8.0f); // 10% for each AR above 8
-		if (approachRateFactor < 8.0f)
-			approachRateFactor -= 0.07f * (approachRateFactor - 8.0f); // -7% for each AR below 8
+			value *= 0.1f * (approachRate - 8.0f); // 10% for each AR above 8
+		if (approachRate < 8.0f)
+			value *= 0.07f * (approachRate - 8.0f); // -7% for each AR below 8
 	}
 
 	// Scale the aim value with accuracy _slightly_
