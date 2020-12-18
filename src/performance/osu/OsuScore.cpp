916f9102f6aba5812275b84282869df7d26dff5c
StanR <hi@stanr.info>
2020-12-18 05:48:27 +0300

    Approximate amount of sliderbreaks in a play

--- src/performance/osu/OsuScore.cpp
+++ src/performance/osu/OsuScore.cpp
@@ -64,11 +64,24 @@ void OsuScore::computeTotalValue(const Beatmap& beatmap)
 		return;
 	}
 
+	// guess the number of misses + slider breaks from combo
+	f32 comboBasedMissCount = 0.0f;
+	f32 beatmapMaxCombo = beatmap.DifficultyAttribute(_mods, Beatmap::MaxCombo);
+	if (beatmap.NumSliders() > 0)
+	{
+		f32 fullComboThreshold = beatmapMaxCombo - 0.1f * beatmap.NumSliders();
+		if (_maxCombo < fullComboThreshold)
+			comboBasedMissCount = fullComboThreshold / _maxCombo;
+		else
+			comboBasedMissCount = std::pow((beatmapMaxCombo - _maxCombo) / (0.1f * beatmap.NumSliders()), 3.0f);
+	}
+	_effectiveMissCount = std::max(_numMiss, std::floor(comboBasedMissCount));
+
 	// Custom multipliers for NoFail and SpunOut.
 	f32 multiplier = 1.12f; // This is being adjusted to keep the final pp value scaled around what it used to be when changing things
 
 	if ((_mods & EMods::NoFail) > 0)
-		multiplier *= std::max(0.9f, 1.0f - 0.02f * _numMiss);
+		multiplier *= std::max(0.9f, 1.0f - 0.02f * _effectiveMissCount);
 
 	int numTotalHits = TotalHits();
 	if ((_mods & EMods::SpunOut) > 0)
@@ -100,8 +113,8 @@ void OsuScore::computeAimValue(const Beatmap& beatmap)
 	_aimValue *= LengthBonus;
 
 	// Penalize misses by assessing # of misses relative to the total # of objects. Default a 3% reduction for any # of misses.
-	if (_numMiss > 0)
-		_aimValue *= 0.97f * std::pow(1.0f - std::pow(_numMiss / static_cast<f32>(numTotalHits), 0.775f), _numMiss);
+	if (_effectiveMissCount > 0)
+		_aimValue *= 0.97f * std::pow(1.0f - std::pow(_effectiveMissCount / static_cast<f32>(numTotalHits), 0.775f), _effectiveMissCount);
 
 	// Combo scaling
 	float maxCombo = beatmap.DifficultyAttribute(_mods, Beatmap::MaxCombo);
@@ -145,8 +158,8 @@ void OsuScore::computeSpeedValue(const Beatmap& beatmap)
 	_speedValue *= lengthBonus;
 
 	// Penalize misses by assessing # of misses relative to the total # of objects. Default a 3% reduction for any # of misses.
-	if (_numMiss > 0)
-		_speedValue *= 0.97f * std::pow(1.0f - std::pow(_numMiss / static_cast<f32>(numTotalHits), 0.775f), std::pow(static_cast<f32>(_numMiss), 0.875f));
+	if (_effectiveMissCount > 0)
+		_speedValue *= 0.97f * std::pow(1.0f - std::pow(_effectiveMissCount / static_cast<f32>(numTotalHits), 0.775f), std::pow(static_cast<f32>(_effectiveMissCount), 0.875f));
 
 	// Combo scaling
 	float maxCombo = beatmap.DifficultyAttribute(_mods, Beatmap::MaxCombo);
